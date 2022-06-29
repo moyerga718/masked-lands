fa47925c33a23dd9bd1408c71b4feb966312fac7
Griffin Moyer <moyerga@outlook.com>
2022-06-29 10:09:45 -0500

    starting character sheet styling

--- src/components/characterSheet/AttributeList.js
+++ src/components/characterSheet/AttributeList.js
@@ -1,10 +1,33 @@
-export const AttributeList = ( {charAtt, charMods, attributeNames}) => {
+import "./AttributeList.css"
+
+export const AttributeList = ({ charAtt, charMods, attributeNames }) => {
 
     const foundModifier = charMods.find(mod => mod.attributeId === charAtt.attributeId)
     const foundAttribute = attributeNames.find(att => att.id === charAtt.attributeId)
     if (charAtt.bonus) {
-        return <p>{foundAttribute?.name}: {charAtt.value} ({foundModifier?.val} mod) *Bonus*</p>
+        return <>
+            <div className="att-and-mod-container">
+                <div className="att-box-boosted">
+                    <p>{charAtt.value}</p>
+                    <p>{foundAttribute?.name}</p>
+                </div>
+                <div className="att-mod-box-boosted">
+                    <p>+{foundModifier?.val}</p>
+                </div>
+            </div>
+        </>
     } else {
-        return <p>{foundAttribute?.name}: {charAtt.value} ({foundModifier?.val} mod)</p>
+        return <>
+            <div className="att-and-mod-container">
+                <div className="att-box">
+                    <p>{charAtt.value}</p>
+                    <p>{foundAttribute?.name}</p>
+                </div>
+                <div className="att-mod-box">
+                    <p>+{foundModifier?.val}</p>
+                </div>
+            </div>
+        </>
+
     }
 }
--- src/components/characterSheet/DevotionList.js
+++ src/components/characterSheet/DevotionList.js
@@ -1,8 +1,15 @@
-export const DevotionList = ( {devotionObj, gods}) => {
+import "./DevotionList.css"
+
+export const DevotionList = ({ devotionObj, gods }) => {
 
     const foundGod = gods.find(god => god.id === devotionObj.godId)
-    
-    return <div>
-        <p><b>{foundGod?.name}:</b> {devotionObj?.devPoints} devotion points (Devotion Level {devotionObj?.devLevel})</p>
+
+    return <div className="devotion-div">
+        <img className="devotion-icon" src={foundGod.iconUrl} />
+        <p><b>{foundGod?.name}</b></p>
+        <div>
+            <p>lvl</p><h3>{devotionObj?.devLevel}</h3>
+        </div>
+        <p> {devotionObj?.devPoints} devotion points </p>
     </div>
 }
