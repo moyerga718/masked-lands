e55933186667aab961d3dc383fdc754f5ea402f6
Griffin Moyer <moyerga@outlook.com>
2022-07-01 14:01:42 -0500

    styled devotion section

--- src/components/newCharacterForm/devotion/GodDevotionInput.js
+++ src/components/newCharacterForm/devotion/GodDevotionInput.js
@@ -4,8 +4,14 @@ import { useState, useEffect } from "react"
 
 export const GodDevotionInput = ({ godObj, charDevotion, setCharDevotion, availableDevPoints, setAvailableDevPoints }) => {
     const [points, setPoints] = useState(0)
+    const [devLevel, setDevLevel] = useState(0)
     const [incrementDisabled, setIncrementDisabled] = useState(false)
     const [decrementDisabled, setDecrementDisabled] = useState(true)
+    const [divStyle, setDivStyle] = useState({
+        borderColor: 'black',
+        boxShadow: '0px 5px 10px 1px rgba(0, 0, 0, 0.15)'
+    })
+    const [boxShadowSpreadVal, setBoxShadowSpreadVal] = useState(1)
 
     //function that increases number of points for this god when increment button is clicked.
     const handleIncrement = () => {
@@ -18,6 +24,64 @@ export const GodDevotionInput = ({ godObj, charDevotion, setCharDevotion, availa
         setPoints(points - 1)
         setAvailableDevPoints(availableDevPoints + 1)
     }
+
+    useEffect(
+        () => {
+            if (points > 0) {
+                setBoxShadowSpreadVal(points * 10)
+            } else {
+                setBoxShadowSpreadVal(1)
+            }
+
+            setDevLevel(findDevotionLevel(points))
+        },
+        [points]
+    )
+
+    useEffect(
+        () => {
+            if (boxShadowSpreadVal > 1) {
+                setDivStyle({
+                    borderColor: `rgba(${godObj?.rgb}, 1)`,
+                    boxShadow: `0px 5px 75px ${boxShadowSpreadVal}px rgba(${godObj?.rgb}, 0.2${points})`
+                })
+            } else {
+                setDivStyle({
+                    borderColor: 'black',
+                    boxShadow: '0px 5px 10px 1px rgba(0, 0, 0, 0.15)'
+                })
+
+            }
+        },
+        [boxShadowSpreadVal]
+    )
+
+    //Function to calculate points level
+
+    const findDevotionLevel = (pointsVal) => {
+        let devLevel = 0
+        if (pointsVal >= 21) {
+            devLevel = 5
+            return devLevel
+        } else if (pointsVal >= 15) {
+            devLevel = 4
+            return devLevel
+        } else if (pointsVal >= 9) {
+            devLevel = 3
+            return devLevel
+        } else if (pointsVal >= 5) {
+            devLevel = 2
+            return devLevel
+        } else if (pointsVal >= 2) {
+            devLevel = 1
+            return devLevel
+        } else {
+            devLevel = 0
+            return devLevel
+        }
+    }
+
+
 
     //useEffect looks out for whether increment/decrement buttons should be disabled.
     //User should not be able to decrement past zero or spend more devotion points than alloted by subclass.
@@ -57,15 +121,26 @@ export const GodDevotionInput = ({ godObj, charDevotion, setCharDevotion, availa
     )
 
     //Return gods name, number of devotion points, increment and decrement buttons.
-    return <div>
-        <p>{godObj?.name}</p>
-        <button onClick={() => handleIncrement()}
-            disabled={incrementDisabled}>
-            +</button>
-        <p>{points}</p>
-        <button onClick={() => handleDecrement()}
-            disabled={decrementDisabled}>
-            -</button>
-
+    return <div className="god-div" style={divStyle}>
+        <div className="god-image-div">
+            <img className="god-image" src={godObj?.imageUrl} />
+        </div>
+        <div className="god-info-div">
+            <h3 className="god-text">{godObj?.name}</h3>
+            <p className="god-text"><i>{godObj?.description}</i></p>
+            <div className="god-devotion-container">
+                <h4 className="god-text">Devotion Points</h4>
+                <div className="god-div-devotion-selection-container">
+                    <button className="devotion-button" onClick={() => handleDecrement()}
+                        disabled={decrementDisabled}>
+                        -</button>
+                    <h4 className="god-div-points-value">{points}</h4>
+                    <button className="devotion-button" onClick={() => handleIncrement()}
+                        disabled={incrementDisabled}>
+                        +</button>
+                </div>
+                <h4 className="god-text">Devotion Level {devLevel}</h4>
+            </div>
+        </div>
     </div>
 }
